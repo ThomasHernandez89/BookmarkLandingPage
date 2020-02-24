a4bb7c543fd81ca0eac7ddef8b2e8a89797849d3
Thomas <34192420+ThomasHernandez89@users.noreply.github.com>
2020-02-24 08:44:26 -0500

    CSS Commit
    Finish burger menu responsiveness and structure

---
+++ script.js
@@ -0,0 +1,11 @@
+function on() {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("_nav_bar_logo").style.display = "none";
+    document.getElementById("_nav_bar_hamb").style.display = "none";
+  }
+  
+  function off() {
+      document.getElementById("overlay").style.display = "none";
+      document.getElementById("_nav_bar_logo").style.display = "block";
+      document.getElementById("_nav_bar_hamb").style.display = "block";
+  }
